07ead6ed7e7e116ba96e9ea7fc7b306da052bb12
joao vitor <joaomakenze@gmail.com>
2025-02-24 20:09:26 -0300

    concluindo a tarefa da EBAC

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -7,4 +7,21 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    test('Deve adicionar "test-1" na lista', () =>{
+        render(<PostComment />)
+        fireEvent.change(screen.getByTestId('campo-tarefa'), {
+            target: { value: 'test-1'}
+        })
+        fireEvent.click(screen.getByTestId('btn-tarefa'))
+        expect(screen.getByText('test-1')).toBeInTheDocument()
+    })
+    test('Deve adicionar "test-2" na lista', () =>{
+        render(<PostComment />)
+        fireEvent.change(screen.getByTestId('campo-tarefa'), {
+            target: { value: 'test-2'}
+        })
+        fireEvent.click(screen.getByTestId('btn-tarefa'))
+        expect(screen.getByText('test-2')).toBeInTheDocument()
+    })
 });
